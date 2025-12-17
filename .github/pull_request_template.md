9c053f12f0be07f0c4a5327376b87e0989b82d04
Stacy Hayes <stacy.hayes@digital.cabinet-office.gov.uk>
2025-12-17 11:32:31 +0000

    initial commit

---
+++ .github/pull_request_template.md
@@ -0,0 +1,1 @@
+This repo is owned by the content modelling team. Please let us know in #govuk-publishing-content-modelling-dev when you raise any PRs.
---
+++ lib/nokodiff.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "nokodiff/version"
+
+module Nokodiff
+  class Error < StandardError; end
+  # Your code goes here...
+end
