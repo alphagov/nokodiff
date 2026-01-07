3a968b3be180d2d6b929551aedd26ef0b2e67af7
Stacy Hayes <stacy.hayes@digital.cabinet-office.gov.uk>
2026-01-07 15:35:28 +0000

    Add character level diffing, wrapping changes with strong highlighting
    Performs individual character diffs inside text nodes. Matching sequences
    buffer normally, not being split out, while any changed, added or deleted
    characters get wrapped with a `<strong>` tag. Because these changed characters
    are logged at character level there is also a method to combine the individual
    `<strong>` tags into blocks to make the page more legible
    
    It then builds replacement Nokogiri fragments for both the original and new
    text nodes, which lets us maintain the original HTML structure, while
    highlighting the specific changed characters.

--- lib/nokodiff.rb
+++ lib/nokodiff.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 require "nokogiri"
+require "diff-lcs"
 
 require_relative "nokodiff/version"
 require_relative "nokodiff/differ"
+require_relative "nokodiff/changes_in_fragments"
 
 module Nokodiff
   def self.diff(before_html, after_html)
