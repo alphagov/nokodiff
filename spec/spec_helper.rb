51bc5b8b27018627b498ce2a5a0ef52d2b7289c6
David Trussler <david.trussler@digital.cabinet-office.gov.uk>
2026-09-03 13:15:13 +0100

    Update `` for final version (WIP)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,15 @@
 # frozen_string_literal: true
 
-# require "rails_helper"
-# require "percy/capybara"
 require "simplecov"
 SimpleCov.start do
   minimum_coverage 100
-  add_filter "lib/nokodiff/engine.rb"
+  skip "lib/nokodiff/engine.rb"
 end
 
 require "nokodiff"
 require "active_support/core_ext/string/output_safety"
 require "active_support/core_ext/string"
 require "rspec-html-matchers"
-# require "url_helpers"
 
 Dir[File.join(File.dirname(__FILE__), "support", "**", "*.rb")].sort.each { |f| require f }
 
@@ -29,9 +26,12 @@ RSpec.configure do |config|
 
   config.include RSpecHtmlMatchers
 
-  config.filter_run_excluding not_applicable: true, visual_regression: true
+  config.filter_run_excluding not_applicable: true # , visual_regression: true
   # Need to treat Dummy as app. See:
   # https://stackoverflow.com/questions/19867202/combining-url-helpers-for-a-rails-engine-and-a-base-application-in-rspec
-  # config.include RSpec.application.routes.url_helpers, type: :feature
-  # config.include Rails.application.routes.url_helpers, type: :feature
+
+  config.before do |example|
+    # Visual regression tests need the JS driver to be used for screenshots
+    Capybara.current_driver = Capybara.javascript_driver if example.metadata[:visual_regression]
+  end
 end
